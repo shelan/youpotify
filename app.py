767ed67ea81a4d2053c4a340375a6642e535710a
shelan <shelanrc@gmail.com>
2015-09-28 00:25:37 +0200

    Changing files and refactoring

--- app.py
+++ app.py
@@ -20,6 +20,11 @@ config = {}
 execfile("oauth.py", config)
 
 
+@app.route('/')
+def home():
+    form = SendMsgForm(request.form)
+    return render_template('forms/send.html',form =form)
+
 
 @app.route('/send', methods=['GET', 'POST'])
 def send():
@@ -40,6 +45,16 @@ def sendMsgToClients(msg):
         form = SendMsgForm(request.form)
     return render_template('forms/send.html', form=form)
 
+
+@app.errorhandler(500)
+def internal_error(error):
+    #db_session.rollback()
+    return render_template('errors/500.html'), 500
+
+
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template('errors/404.html'), 404
 
 if __name__ == "__main__":
     app.run()
