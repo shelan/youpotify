4065edf0fa751ef252774f64d9e9524deb0e5138
shelan <shelanrc@gmail.com>
2015-10-08 21:11:46 +0200

    adding new oauth

--- oauth.py
+++ oauth.py
@@ -9,7 +9,7 @@
 # access_key = "15259428-3xd7uvAo7edhMBOQkwaTlxaAsOpTFnNlKVhFqmMR8"
 # access_secret = "q5rAULbSbm15uOb25PLNHo2uVvyvFUZFBRKY8c8zkq9wj"
 
-consumer_key = 'qKM6FVhwZzfGZ8zL7YuAyvELc'
-consumer_secret = 'sC8vx1ykvo8YMfTPcih7enHpr8WLmLnbK4kh8udq7Lle3po7VT'
-access_key = '3769602317-905kaexMdOawoB5QoM3LtGXCM14K25dHRjiZZ4E'
-access_secret = 'dZTpRhGGnL5wgK5PMm5r8BJ2vXxWZRi7UUrvZskNMlypS'
+consumer_key = 'c3WfzWemjUm2K0DCEsnheR8O0'
+consumer_secret = 'EI1Zl1SPpb8SrnNH4XqKfBX7dw4LjlmFydgm6SPNr6ILLI5SaT'
+access_key = '1665548742-2L48xwqnH6Rsp1AMINZCpFAwNT64tMH0LebqQtX'
+access_secret = 'ty5aU1BZqjIQaHO3HjvEpqTWZK0KabvhbJw444zHpppLk'
