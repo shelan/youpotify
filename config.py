ab52db5885db197d38c65b8482a639317a013548
Shelan Perera <shelanrc@gmail.com>
2016-12-02 12:54:28 +0100

    Update config.py

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'M0094QU0Ge3YMjUWK4b2WA1037lcLRJ2J8s179NG'
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'database.db')
 
 
-CONSUMER_KEY = 'c3WfzWemjUm2K0DCEsnheR8O0'
-CONSUMER_SECRET = 'EI1Zl1SPpb8SrnNH4XqKfBX7dw4LjlmFydgm6SPNr6ILLI5SaT'
-ACCESS_KEY = '1665548742-2L48xwqnH6Rsp1AMINZCpFAwNT64tMH0LebqQtX'
-ACCESS_SECRET = 'ty5aU1BZqjIQaHO3HjvEpqTWZK0KabvhbJw444zHpppLk'
+CONSUMER_KEY = '<add your key>'
+CONSUMER_SECRET = '<add your secrect>'
+ACCESS_KEY = '<add your key>'
+ACCESS_SECRET = '<add your secrect>'
